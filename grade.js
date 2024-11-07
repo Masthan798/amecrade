ff1cb91ed6302ee93a63ebe8a503837c07823132
Masthan798 <masthannaghamunthala01.com>
2024-11-07 10:23:59 +0530

    first commit

---
+++ grade.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+    $('.your-class').slick({
+        slidesToShow: 9,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed:7,
+        cssEase:'linear',  
+        arrows:false,    
+    });
+  });
+
+  $(document).ready(function(){
+    $('.your-class1').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed:2000,
+        cssEase:'linear', 
+        arrows:false,
+    });
+  });
+
+  $(document).ready(function(){
+    $('.your-class2').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed:900,
+        cssEase:'linear', 
+        arrows:false,
+    });
+  });
+
+  $(document).ready(function(){
+    $('.your-class3').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed:900,
+        cssEase:'linear', 
+        arrows:false,
+    });
+  });
+  
+
+  
+              
+
+
+
+      
